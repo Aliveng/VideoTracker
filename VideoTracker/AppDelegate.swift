c8a8a5960530d0349f0f972cf59010831200b5f5
Aliveng <aliveng@yandex.ru>
2020-07-16 11:07:51 +0300

    Initial commit

---
+++ VideoTracker/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  VideoTracker
+//
+//  Created by Татьяна Севостьянова on 16.07.2020.
+//  Copyright © 2020 Татьяна Севостьянова. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow()
+        self.window = window
+        
+        window.rootViewController = UINavigationController(rootViewController: AuthorizationViewController())
+        window.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ VideoTracker/AuthorizationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoTracker
+//
+//  Created by Татьяна Севостьянова on 16.07.2020.
+//  Copyright © 2020 Татьяна Севостьянова. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
